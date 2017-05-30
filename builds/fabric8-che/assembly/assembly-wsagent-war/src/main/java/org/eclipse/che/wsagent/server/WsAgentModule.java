aae920915cf8474b06dc1ad1303fd1e4224ae456
Sun Seng David Tan <sutan@redhat.com>
2017-05-30 13:08:18 +0000

    CHE-229 rebase che: sync WsAgentModule guice configuration from upstream
    Signed-off-by: Sun Seng David Tan <sutan@redhat.com>

--- builds/fabric8-che/assembly/assembly-wsagent-war/src/main/java/org/eclipse/che/wsagent/server/WsAgentModule.java
+++ builds/fabric8-che/assembly/assembly-wsagent-war/src/main/java/org/eclipse/che/wsagent/server/WsAgentModule.java
@@ -10,89 +10,44 @@
  *******************************************************************************/
 package org.eclipse.che.wsagent.server;
 
-import com.google.gson.JsonParser;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import com.google.inject.assistedinject.FactoryModuleBuilder;
-import com.google.inject.name.Names;
 import com.redhat.che.keycloak.server.KeycloakHttpJsonRequestFactory;
 
-import org.eclipse.che.ApiEndpointAccessibilityChecker;
-import org.eclipse.che.EventBusURLProvider;
-import org.eclipse.che.UriApiEndpointProvider;
-import org.eclipse.che.UserTokenProvider;
-import org.eclipse.che.api.auth.oauth.OAuthTokenProvider;
-import org.eclipse.che.api.core.jsonrpc.BuildingRequestTransmitter;
-import org.eclipse.che.api.core.jsonrpc.JsonRpcFactory;
-import org.eclipse.che.api.core.jsonrpc.JsonRpcMessageReceiver;
-import org.eclipse.che.api.core.jsonrpc.RequestHandlerConfigurator;
 import org.eclipse.che.api.core.rest.ApiInfoService;
-import org.eclipse.che.api.core.rest.CoreRestModule;
 import org.eclipse.che.api.core.rest.HttpJsonRequestFactory;
-import org.eclipse.che.api.core.util.FileCleaner.FileCleanerModule;
-import org.eclipse.che.api.core.websocket.WebSocketMessageReceiver;
-import org.eclipse.che.api.core.websocket.WebSocketMessageTransmitter;
-import org.eclipse.che.api.core.websocket.impl.BasicWebSocketMessageTransmitter;
-import org.eclipse.che.api.core.websocket.impl.GuiceInjectorEndpointConfigurator;
-import org.eclipse.che.api.git.GitConnectionFactory;
-import org.eclipse.che.api.git.GitUserResolver;
-import org.eclipse.che.api.git.LocalGitUserResolver;
-import org.eclipse.che.api.project.server.ProjectApiModule;
-import org.eclipse.che.api.ssh.server.HttpSshServiceClient;
-import org.eclipse.che.api.ssh.server.SshServiceClient;
-import org.eclipse.che.api.user.server.spi.PreferenceDao;
 import org.eclipse.che.commons.lang.Pair;
-import org.eclipse.che.git.impl.jgit.JGitConnectionFactory;
 import org.eclipse.che.inject.DynaModule;
-import org.eclipse.che.plugin.java.server.rest.WsAgentURLProvider;
-import org.eclipse.che.security.oauth.RemoteOAuthTokenProvider;
+
 
 import javax.inject.Named;
-import javax.inject.Singleton;
-import java.net.URI;
 
 /**
+ * Mandatory modules of workspace agent
+ *
  * @author Evgen Vidolob
+ * @author Sergii Kabashniuk
  */
 @DynaModule
 public class WsAgentModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(ApiInfoService.class);
-
-
-        bind(PreferenceDao.class).to(org.eclipse.che.RemotePreferenceDao.class);
-
-        bind(OAuthTokenProvider.class).to(RemoteOAuthTokenProvider.class);
-        bind(SshServiceClient.class).to(HttpSshServiceClient.class);
-
-        bind(org.eclipse.che.plugin.ssh.key.script.SshKeyProvider.class)
-                .to(org.eclipse.che.plugin.ssh.key.script.SshKeyProviderImpl.class);
-
-        install(new CoreRestModule());
-        install(new FileCleanerModule());
-        install(new ProjectApiModule());
-        install(new org.eclipse.che.swagger.deploy.DocsModule());
-        install(new org.eclipse.che.api.debugger.server.DebuggerModule());
+        install(new org.eclipse.che.security.oauth.OAuthAgentModule());
+        install(new org.eclipse.che.api.core.rest.CoreRestModule());
+        install(new org.eclipse.che.api.core.util.FileCleaner.FileCleanerModule());
+        install(new org.eclipse.che.api.project.server.ProjectApiModule());
         install(new org.eclipse.che.commons.schedule.executor.ScheduleModule());
+        install(new org.eclipse.che.plugin.ssh.key.SshModule());
+        install(new org.eclipse.che.api.languageserver.LanguageServerModule());
+        install(new org.eclipse.che.api.debugger.server.DebuggerModule());
+        install(new org.eclipse.che.api.git.GitModule());
+        install(new org.eclipse.che.git.impl.jgit.JGitModule());
+        install(new org.eclipse.che.api.core.jsonrpc.impl.JsonRpcModule());
+        install(new org.eclipse.che.api.core.websocket.impl.WebSocketModule());
 
-        bind(GitUserResolver.class).to(LocalGitUserResolver.class);
-        bind(GitConnectionFactory.class).to(JGitConnectionFactory.class);
-
-        bind(URI.class).annotatedWith(Names.named("che.api")).toProvider(UriApiEndpointProvider.class);
-        bind(String.class).annotatedWith(Names.named("user.token")).toProvider(UserTokenProvider.class);
-
-        bind(String.class).annotatedWith(Names.named("event.bus.url")).toProvider(EventBusURLProvider.class);
-        bind(ApiEndpointAccessibilityChecker.class);
-        bind(WsAgentAnalyticsAddresser.class);
-
-        bind(String.class).annotatedWith(Names.named("wsagent.endpoint"))
-                          .toProvider(WsAgentURLProvider.class);
-                       
         bind(HttpJsonRequestFactory.class).to(KeycloakHttpJsonRequestFactory.class);
 
-        configureJsonRpc();
-        configureWebSocket();
     }
 
     //it's need for WSocketEventBusClient and in the future will be replaced with the property
@@ -109,24 +64,5 @@ public class WsAgentModule extends AbstractModule {
     @SuppressWarnings("unchecked")
     Pair<String, String>[] propagateEventsProvider(@Named("event.bus.url") String eventBusURL) {
         return new Pair[]{Pair.of(eventBusURL, "")};
-    }
-
-    private void configureWebSocket() {
-        requestStaticInjection(GuiceInjectorEndpointConfigurator.class);
-        bind(WebSocketMessageTransmitter.class).to(BasicWebSocketMessageTransmitter.class);
-
-        bind(WebSocketMessageReceiver.class).to(JsonRpcMessageReceiver.class);
-    }
-
-    private void configureJsonRpc() {
-        install(new FactoryModuleBuilder().build(JsonRpcFactory.class));
-        install(new FactoryModuleBuilder().build(RequestHandlerConfigurator.class));
-        install(new FactoryModuleBuilder().build(BuildingRequestTransmitter.class));
-    }
-
-    @Provides
-    @Singleton
-    public JsonParser jsonParser(){
-        return new JsonParser();
     }
 }
