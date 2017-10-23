72c05f684d80c18c355736dbfca31a040da0b8bc
David Festal <dfestal@redhat.com>
2017-10-23 15:22:59 +0200

    Fix the ci tagging script + update `che.version` to `5.20.0-SNAPSHOT` (#384)
    Signed-off-by: David Festal <dfestal@redhat.com>

--- .ci/cico_do_docker_build_tag_push.sh
+++ .ci/cico_do_docker_build_tag_push.sh
@@ -60,9 +60,10 @@ do
   docker rmi eclipse/che-server:local
 
   # lets change the tag and push it to the registry
-  docker tag ${DOCKER_HUB_NAMESPACE}/che-server:${NIGHTLY} ${DOCKER_HUB_NAMESPACE}/che-server:${TAG}
-  
-  dockerTags="${dockerTags} ${DOCKER_HUB_NAMESPACE}/che-server:${NIGHTLY} ${DOCKER_HUB_NAMESPACE}/che-server:${TAG}"
+  docker tag ${DOCKER_HUB_NAMESPACE}/che-server:${NIGHTLY} ${DOCKER_HUB_NAMESPACE}/che-server-multiuser:${TAG}
+  docker tag ${DOCKER_HUB_NAMESPACE}/che-server:${NIGHTLY} ${DOCKER_HUB_NAMESPACE}/che-server-multiuser:${NIGHTLY}
+    
+  dockerTags="${dockerTags} ${DOCKER_HUB_NAMESPACE}/che-server-multiuser:${NIGHTLY} ${DOCKER_HUB_NAMESPACE}/che-server-multiuser:${TAG}"
     
   if [ "$DeveloperBuild" != "true" ]
   then
