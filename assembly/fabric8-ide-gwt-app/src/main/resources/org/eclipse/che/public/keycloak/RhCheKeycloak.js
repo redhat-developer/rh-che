608152338fe44762ac2c694c9ad45ff5cc9d507c
Angel Misevski <amisevsk@redhat.com>
2018-08-20 10:16:05 -0400

    Update upstream to 6.9.0, update license headers
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- assembly/fabric8-ide-gwt-app/src/main/resources/org/eclipse/che/public/keycloak/RhCheKeycloak.js
+++ assembly/fabric8-ide-gwt-app/src/main/resources/org/eclipse/che/public/keycloak/RhCheKeycloak.js
@@ -1,14 +1,14 @@
 /*
  * Copyright (c) 2016-2018 Red Hat, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-
 const osio_msg_provisioning = "Creating your <strong>OpenShift</strong> account";
 const osio_msg_linking_account = "Linking your <strong>OpenShift</strong> account";
 const osio_msg_setting_up_namespaces = "Setting up your <strong>OpenShift.io</strong> environment";
