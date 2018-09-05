21cfb40d1625d361beb179038b8262c1a9fa4ac5
fabric8-cd <fabric8cd@gmail.com>
2018-09-05 20:40:11 +0100

    fix(version): update fabric8-analytics-lsp-server to 0.1.32

--- plugins/ls-bayesian-agent/src/main/resources/installers/1.0.0/com.redhat.bayesian.lsp.script.sh
+++ plugins/ls-bayesian-agent/src/main/resources/installers/1.0.0/com.redhat.bayesian.lsp.script.sh
@@ -12,7 +12,7 @@ CHE_DIR=$HOME/che
 LS_DIR=${CHE_DIR}/ls-bayesian
 LS_LAUNCHER=${LS_DIR}/launch.sh
 
-AGENT_BINARIES_URI=https://github.com/fabric8-analytics/fabric8-analytics-lsp-server/releases/download/0.0.5/ca-lsp-server.tar
+AGENT_BINARIES_URI=https://github.com/fabric8-analytics/fabric8-analytics-lsp-server/releases/download/v0.1.32/ca-lsp-server.tar
 
 
 if [ -f /etc/centos-release ]; then
