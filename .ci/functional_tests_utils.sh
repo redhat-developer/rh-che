fa7ebb8ecdaf1ccd03fbb3a1d4b1db64dc7aa4a5
Radim Hopp <rhopp@redhat.com>
2019-03-11 17:47:00 +0100

    Add target/failsafe-reports to directories to archive

--- .ci/functional_tests_utils.sh
+++ .ci/functional_tests_utils.sh
@@ -98,5 +98,6 @@ function archiveArtifacts() {
   mkdir -p ./rhche/${JOB_NAME}/${BUILD_NUMBER}/surefire-reports
   cp ./logs/*.log ./rhche/${JOB_NAME}/${BUILD_NUMBER}/
   cp -R ./logs/artifacts/screenshots/ ./rhche/${JOB_NAME}/${BUILD_NUMBER}/
+	cp -R ./logs/artifacts/failsafe-reports/ ./rhche/${JOB_NAME}/${BUILD_NUMBER}/
   rsync --password-file=./artifacts.key -PHva --relative ./rhche/${JOB_NAME}/${BUILD_NUMBER} devtools@artifacts.ci.centos.org::devtools/
 }
