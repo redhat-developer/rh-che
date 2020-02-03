7b5003c7fed355f60e7d38dee20a6932e7bda38f
Vitalii Parfonov <vparfono@redhat.com>
2020-02-03 13:44:11 +0200

    Add build script for job which will checking compatibility build for RedHat Che and Eclipse Che (upstream)
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

---
+++ .ci/cico_compatibility_build_check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+yum -y update
+yum -y install centos-release-scl-rh java-1.8.0-openjdk-devel git 
+yum -y install rh-maven33
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+curl -sL https://rpm.nodesource.com/setup_10.x | bash -
+yum-config-manager --add-repo https://dl.yarnpkg.com/rpm/yarn.repo
+yum install -y docker-ce nodejs yarn gcc-c++ make
+service docker start
+set -e
+
+git rebase origin/master || (echo "ERROR: Rebasing branch 'upstream' on top of 'master' failed"; exit 1)
+
+CHE_VERSION=$(curl -s https://raw.githubusercontent.com/eclipse/che/master/pom.xml | grep "^    <version>.*</version>$" | awk -F'[><]' '{print $3}')
+echo ">>> change upstream version to: $CHE_VERSION"
+scl enable rh-maven33 'mvn versions:update-parent  versions:commit -DallowSnapshots=true -DparentVersion=[$CHE_VERSION] -Dmaven.repo.local=${WORKSPACE}/.repository -U'
+scl enable rh-maven33 'mvn clean install -Dmaven.repo.local=${WORKSPACE}/.repository -U'
