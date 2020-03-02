2cbd8e590f48ae18781607c27510320572ab4f90
kkanova <kkanova@redhat.com>
2020-03-02 14:53:32 +0100

    Add custom reporter, add rh-che token handler.

--- e2e-saas/inversify.config.ts
+++ e2e-saas/inversify.config.ts
@@ -8,16 +8,19 @@
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
 import { Container } from 'inversify';
-import { inversifyConfig, TYPES, ICheLoginPage, ITestWorkspaceUtil, CLASSES } from 'e2e';
+import { inversifyConfig, TYPES, ICheLoginPage, ITestWorkspaceUtil } from 'e2e';
 import { RhCheLoginPage } from './pageobjects/RhCheLoginPage';
 import { RhCheTestWorkspaceUtils } from './utils/RhCheTestWorkspaceUtils';
+import { RhCheTokenHandler } from './utils/RhCheTokenHandler';
 
 const rhcheContainer: Container = inversifyConfig.e2eContainer;
 
 rhcheContainer.unbind(TYPES.CheLogin);
-rhcheContainer.bind<ICheLoginPage>(TYPES.CheLogin).to(RhCheLoginPage).inSingletonScope();
+rhcheContainer.bind<ICheLoginPage>(TYPES.CheLogin).to(RhCheLoginPage);
 
 rhcheContainer.unbind(TYPES.WorkspaceUtil);
-rhcheContainer.bind<ITestWorkspaceUtil>(TYPES.WorkspaceUtil).to(RhCheTestWorkspaceUtils).inSingletonScope();
+rhcheContainer.bind<ITestWorkspaceUtil>(TYPES.WorkspaceUtil).to(RhCheTestWorkspaceUtils);
+
+rhcheContainer.rebind(TYPES.ITokenHandler).to(RhCheTokenHandler);
 
 export { rhcheContainer };
