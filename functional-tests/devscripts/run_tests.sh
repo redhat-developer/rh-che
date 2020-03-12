7833e8158df10f1498c3866399e94bb3266e072a
Katerina Foniok <kkanova@redhat.com>
2020-03-12 07:53:41 +0100

    Create PR check specific test suite containing just Java Maven test. (#1826)

--- functional-tests/devscripts/run_tests.sh
+++ functional-tests/devscripts/run_tests.sh
@@ -126,7 +126,7 @@ if [[ "$PR_CHECK_BUILD" == "true" ]]; then
      -e USERNAME=$RH_CHE_AUTOMATION_CHE_PREVIEW_USERNAME \
      -e PASSWORD=$RH_CHE_AUTOMATION_CHE_PREVIEW_PASSWORD \
      -e URL=http://$HOST_URL \
-     -e TEST_SUITE=test-all \
+     -e TEST_SUITE=pr-check \
      -e TS_SELENIUM_LOAD_PAGE_TIMEOUT=180000 \
      --shm-size=256m \
   $rhche_image
