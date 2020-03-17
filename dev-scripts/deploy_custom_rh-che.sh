80116eac270b03e894653e960f66ab3180b94095
Ilya Buziuk <ibuziuk@redhat.com>
2020-03-17 18:52:22 +0100

    Removing workaround with the emptyDir volumes for postgres deployment
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- dev-scripts/deploy_custom_rh-che.sh
+++ dev-scripts/deploy_custom_rh-che.sh
@@ -276,13 +276,6 @@ if ! (curl -L0fs https://raw.githubusercontent.com/eclipse/che/master/deploy/ope
   rm postgres-template.yaml
 fi
 
-# Due to bug in dev-cluster, volume must be changed to emptyDir
-# https://gitlab.cee.redhat.com/dtsd/housekeeping/issues/2570
-if [ "$RH_CHE_OPENSHIFT_URL" == "https://api.che-dev.x6e0.p1.openshiftapps.com:6443" ]; then
-  cp postgres-template.yaml postgres-template_tmp.yaml
-  yq --yaml-output 'del(.objects[0].spec.template.spec.volumes[0].persistentVolumeClaim) | .objects[0].spec.template.spec.volumes[0] += {"emptyDir":{}}' postgres-template_tmp.yaml > postgres-template.yaml
-fi
-
 cd ../ || exit 1
 
 # GET CHE-APP CONFIGS
